37a025c0e698dd27eb7d90b79224ac2b467c7e07
aaron <aaronlyn88@gmail.com>
2024-07-10 15:55:42 +0800

    chore: update eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -85,12 +85,12 @@ module.exports = {
       ],
       rules: {
         "@typescript-eslint/no-unused-vars": "warn",
-        "@typescript-eslint/no-explicit-any": "warn",
+        "@typescript-eslint/no-explicit-any": "error",
         // "@typescript-eslint/explicit-module-boundary-types": "off",
         // "@typescript-eslint/no-var-requires": "off",
         "@typescript-eslint/no-empty-function": "off",
         "@typescript-eslint/no-namespace": "off",
-        "@typescript-eslint/no-non-null-assertion": "off",
+        "@typescript-eslint/no-non-null-assertion": "warn",
       }
     },
 
